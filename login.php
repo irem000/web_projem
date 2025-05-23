da23d1d13b0a1735101d37177b856b7e3a30c5ff
irem000 <irem54ece@gmail.com>
2025-05-23 21:31:59 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -9,24 +9,27 @@ $sifre = $_POST['sifre'] ?? '';
 
 // Mail adresi kontrolü
 function gecerliSakaryaMail($mail) {
-    return preg_match('/^[a-z0-9._%+-]+@sakarya\.edu\.tr$/', $mail);
+    return preg_match('/^[a-z0-9._%+-]+@sakarya\.edu\.tr$/', $mail); // Sadece @sakarya.edu.tr uzantılı mailleri kabul edecek
 }
 
-// Giriş bilgileri dolu mu kontrol et
-if (empty($kullanici) || empty($sifre) || !gecerliSakaryaMail($kullanici)) {
-    header("Location: login.html");
+// Giriş bilgileri(kullanıcı adı - şifre) dolu mu kontrol et
+if (empty($kullanici) || empty($sifre) || !gecerliSakaryaMail($kullanici)) { // HTML5'in required özelliğinden dolayı uyarı mesajı tarayıcı tarafından otomatik oluşturulur
+    header("Location: login.html"); // Tekrar login sayfası yöndendirilir
     exit;
 }
 
-// Kullanıcı adı doğruysa şifre kısmı mail'in başındaki öğrenci numarası olmalı
-$dogru_sifre = explode('@', $kullanici)[0];
+$dogru_sifre = explode('@', $kullanici)[0];// Mail adresinin @ öncesi kısmı, yani öğrenci numarası, şifre olmalıdır
 
 // Doğrulama
 if ($sifre === $dogru_sifre) {
     echo "<h2 style='text-align:center; margin-top:100px;'>Hoşgeldiniz <strong>$sifre</strong>! Giriş başarılı.</h2>";
-    echo "<div style='text-align:center; margin-top:20px;'><a href='webOdev.html' class='btn btn-success'>Ana Sayfa</a></div>";
+    echo "<div style='text-align:center; margin-top:20px;'><a href='main.html' class='btn btn-success'>Ana Sayfa</a></div>";
 } else {
-    header("Location: login.html");
-    exit;
+    echo "<script>
+  alert('Hatalı giriş! Lütfen bilgilerinizi kontrol edin.');
+  window.location.href = 'login.html';
+</script>";
+exit;
+
 }
 ?>
